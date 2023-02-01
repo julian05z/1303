5845994e05e478c385439ccfa0023088ee483363
Antonio <antonio.neuweiler@bluewin.ch>
2023-02-01 14:28:11 +0100

    Daten

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 const express = require ('express')
 const app = express();
 
-const users = [
-    {id: 1, name: "Peter Burkard"},
-    {id: 2, name: "Janin Möckel"},
-    {id: 3, name: "Nicola Süss"},
-    {id: 4, name: "Leano Hunzicker"},
-    {id: 5, name: "Mila Jud"},
-    {id: 5, name: "Mia Mutard"},
-    
-];
 
-app.get('/',(req,res) => {
+
+
+app.get('/users',(req,res) => {
     res.send("Hello World")
 });
 
